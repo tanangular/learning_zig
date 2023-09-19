528be867db7d8518af0ea57d27fe07cc49424ebb
tanangular <tanangular@gmail.com>
2023-09-19 15:44:43 +0700

    เพิ่มตัวอย่าง print แบบมี padding

--- src/01_hello_world.zig
+++ src/01_hello_world.zig
@@ -2,4 +2,7 @@ const std = @import("std");
 
 pub fn main() !void {
   std.debug.print("Hello {s}.\n", .{ "world" });
+
+  // with padding: 20 spaces
+  std.debug.print("Hello {s:20}.\n", .{ "world" });
 }
